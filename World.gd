24e934de426a43525ac5cd87ce836301650b1bed
Remi Rampin <r@remirampin.com>
2020-05-10 23:04:20 -0400

    Factor local player creation

--- World.gd
+++ World.gd
@@ -3,25 +3,32 @@ extends Spatial
 var Character = preload("res://Character.tscn")
 var Player = preload("res://Player.tscn")
 
-func _ready():
-	var character1 = Character.instance()
-	character1.translation = Vector3(-3.0, 0.0, 0.0)
-	character1.color = Color(0.25, 0.25, 1.0)
-	add_child(character1)
-	var player1 = Player.instance()
-	player1.controls = "key1"
-	add_child(player1)
-	character1.controller = player1
-	player1.anchor_top = 0.0
-	player1.anchor_bottom = 0.5
+var local_players = []
 
-	var character2 = Character.instance()
-	character2.translation = Vector3(3.0, 0.0, 0.0)
-	character1.color = Color(1.0, 0.25, 0.25)
-	add_child(character2)
-	var player2 = Player.instance()
-	player2.controls = "key2"
-	add_child(player2)
-	character2.controller = player2
-	player2.anchor_top = 0.5
-	player2.anchor_bottom = 1.0
+func random_color():
+	return Color(rand_range(0.25, 1.0), rand_range(0.25, 1.0), rand_range(0.25, 1.0))
+
+func random_position():
+	return Vector3(rand_range(-10, 10.0), 0.0, rand_range(-10, 10.0))
+
+func add_local_player(controls):
+	var character = Character.instance()
+	character.translation = random_position()
+	character.color = random_color()
+	add_child(character)
+	var player = Player.instance()
+	player.controls = controls
+	add_child(player)
+	character.controller = player
+
+	local_players.append(player)
+
+	for i in len(local_players):
+		local_players[i].anchor_top = 1.0 / len(local_players) * i
+		local_players[i].anchor_bottom = 1.0 / len(local_players) * (i + 1)
+
+func _ready():
+	randomize()
+
+	add_local_player("key1")
+	add_local_player("key2")
